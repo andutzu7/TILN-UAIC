6010747fd24ab8c7ac130bfb639bc3014c95eb07
andutzu7 <rockkidd57@gmail.com>
2021-04-17 19:06:29 +0300

    updated the corpus/balanced the dataset

--- Resources/flascc/wsdl_calls.py
+++
@@ -1,7 +0,0 @@
-import zeep
-
-wsdl = 'http://nlptools.info.uaic.ro/WebPosRo/PosTaggerRoWS?wsdl'
-client = zeep.Client(wsdl=wsdl)
-response = ''
-response += client.service.parseSentence_XML('ana arer mere.')
-print(response)
